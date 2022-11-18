c845ea5853b249ad84e736cfed3df914e7b16618
Gustavo Felix <gustavofelixssilva@gmail.com>
2022-11-18 15:12:24 -0300

    atualizando coisas

--- src/pages/contact/style.js
+++ src/pages/contact/style.js
@@ -1,13 +1,13 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   height: calc(100vh - 100px);
 `;
 
-export const Content = styled.div`
+ export const Content = styled.div`
   width: 1120px;
   height: 100%;
   display: flex;
